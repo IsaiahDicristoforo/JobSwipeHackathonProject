8e61f434194d029347fd96164ffee7258f775189
Isaiah Dicristoforo <48969369+IsaiahDicristoforo@users.noreply.github.com>
2021-02-20 18:29:31 -0500

    Initial commit

---
+++ app/src/main/java/edu/uc/groupproject/jobswipe/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.uc.groupproject.jobswipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.uc.groupproject.jobswipe.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/edu/uc/groupproject/jobswipe/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package edu.uc.groupproject.jobswipe.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
